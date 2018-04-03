27eaba38219ce6d2b6424f11396193a162ee3360
Jianhao Song <>
2018-04-03 15:52:32 -0700

    no message

--- PluginzApp/StaysModule/Source/StayCardViewController.swift
+++ PluginzApp/StaysModule/Source/StayCardViewController.swift
@@ -8,13 +8,27 @@
 
 import UIKit
 
-class StayCardViewController: UIViewController {
+class StayCardCell: UICollectionViewCell {
+    
+    @IBOutlet var iconImageView: UIImageView!
+    @IBOutlet var titleLabel: UILabel!
+    
+}
+
+class StayCardViewController: UICollectionViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return collectionView.dequeueReusableCell(withReuseIdentifier: "StayCardCell", for: indexPath)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        guard let cell = cell as? StayCardCell else { return }
+    }
+    
 }
